c6aa2bcb2c5db50bcc493df41ab3ff60d52cabcd
Manmohan Kinthali <manmohankinthali@gmail.com>
2022-04-09 11:18:19 +0530

    made gpio specific for this LED flash mechanism

--- src/drivers/gpio.c
+++ src/drivers/gpio.c
@@ -10,6 +10,9 @@ gpio_init(void)
    /* ADP1650_ENABLE_PIN */
    GPIOA->PDDR |= ( OUTPUT << PIN_A7 );
 
+   /* ADP1650_STROBE_PIN */
+   GPIOA->PDDR |= ( OUTPUT << PIN_A6 );
+
    /* I2C_SDA_LINE */
    GPIOB->PDDR |= ( INPUT << PIN_B6 );
 
@@ -17,5 +20,27 @@ gpio_init(void)
    GPIOB->PDDR |= (OUTPUT << PIN_B7 );
 }
 
+void
+gpio_clear_enable_pin(void)
+{
+   GPIOA->PCOR |= ( 1 << PIN_A7 );
+}
 
+void
+gpio_set_enable_pin(void)
+{
+   GPIOA->PSOR |= ( 1 << PIN_A7 );
+}
+
+void
+gpio_set_strobe_pin(void)
+{
+   GPIOA->PSOR |= ( 1 << PIN_A7 );
+}
+
+void
+gpio_clear_strobe_pin(void)
+{
+   GPIOA->PCOR |= ( 1 << PIN_A6 );
+}
 
--- src/drivers/gpio.h
+++ src/drivers/gpio.h
@@ -6,30 +6,28 @@
 #define OUTPUT 1
 #define INPUT 0
 
-
 #define PORT_A A
 #define PORT_B A
 
-
+#define PIN_A6 6
 #define PIN_A7 7
 #define PIN_B6 14
 #define PIN_B7 15
 
 
 
-
-
-#define OUTPUT_SET(port,register_num) \
-  FGPIO##port##_PSOR |=1<<register_num
-
-#define OUTPUT_CLEAR(port,register_num) \
-  FGPIO##port##_PCOR |=1<<register_num
-
-#define OUTPUT_TOGGLE(port,register_num) \
-  FGPIO##port##_PTOR |=1<<register_num
-
-
 void
 gpio_init(void);
 
+void
+gpio_clear_enable_pin(void);
+
+void
+gpio_set_enable_pin(void);
+
+void
+gpio_set_strobe_pin(void);
+
+void
+gpio_clear_strobe_pin(void);
 
